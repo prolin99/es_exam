2beb57fa1595a018606849a9309aec33005b7ea8
prolin99 <prolin99@gmail.com>
2014-09-30 21:50:22 +0800

    配合 switch

--- admin/ajax_set_exam_data.php
+++ admin/ajax_set_exam_data.php
@@ -6,7 +6,11 @@ include_once "../function.php";
 /*-----------function區--------------*/
 //資料檢查
 $_GET['id'] = intval($_GET['id']) ;
+if ($_GET['id_mode']  == 'upload' ) 
+	$_GET['do']='upload' ;	
 
+if ($_GET['id_mode']  == 'openshow' ) 
+	$_GET['do']='show' ;
 
 switch($_GET['do']){
 	//刪除資料
@@ -16,15 +20,24 @@ switch($_GET['do']){
 		$xoopsDB->queryF($sql) ;
 	break;
 	case "upload";
-		$_GET['setdata'] = intval($_GET['setdata']) ;
-  		$sql = "update ".$xoopsDB->prefix("exam")." set    `upload_mode` = '{$_GET['setdata']}'   where assn='{$_GET['id']}'     " ;
+		if ($_GET['setdata']  === 'true' )
+			$setdata =1 ;
+		else 
+			$setdata =0 ;	
+
+  		$sql = "update ".$xoopsDB->prefix("exam")." set    `upload_mode` = '$setdata'   where assn='{$_GET['id']}'     " ;
   		$xoopsDB->queryF($sql) ;
 	break;
 	case "show";
-		$_GET['setdata'] = intval($_GET['setdata']) ;
-  		$sql = "update ".$xoopsDB->prefix("exam")." set    `open_show` = '{$_GET['setdata']}'   where assn='{$_GET['id']}'     " ;
+
+		//$_GET['setdata'] = intval($_GET['setdata']) ;
+		if ($_GET['setdata']  === 'true' )
+			$setdata =1 ;
+		else 
+			$setdata =0 ;			
+  		$sql = "update ".$xoopsDB->prefix("exam")." set    `open_show` = '$setdata'   where assn='{$_GET['id']}'     " ;
   		$xoopsDB->queryF($sql) ;
 	break;
 
 }	
-//echo $sql ;
+//echo $sql . $_GET['setdata']  ;

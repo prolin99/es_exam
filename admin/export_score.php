4a8b681ff759a8cc73b3569aadb11082813d8af4
prolin99 <prolin99@gmail.com>
2023-07-30 09:31:36 +0800

    修正 PHPExcel 所在目錄

--- admin/export_score.php
+++ admin/export_score.php
@@ -8,8 +8,9 @@
 include_once "header.php";
 include_once "../function.php";
 
-include_once "../../tadtools/PHPExcel.php";
-require_once '../../tadtools/PHPExcel/IOFactory.php';
+require_once XOOPS_ROOT_PATH . '/modules/tadtools/vendor/phpoffice/phpexcel/Classes/PHPExcel.php'; //引入 PHPExcel 物件庫
+require_once XOOPS_ROOT_PATH . '/modules/tadtools/vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php'; //引入PHPExcel_IOFactory 物件庫
+
 /*-----------function區--------------*/
 
 
